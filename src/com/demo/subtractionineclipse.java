6ad14483b027965c967eb37c60c13ae7c989e85c
Girish1369 <girishkatkamwar.1369@gmail.com>
2023-05-26 21:55:03 +0530

    subtraction from local

---
+++ src/com/demo/subtractionineclipse.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class subtractionineclipse {
+
+	public static void main(String[] args) {
+		//  by push operation 
+		int a=20;
+		int b=10;
+		System.out.println("subtraction="+(a-b));
+
+	}
+
+}
